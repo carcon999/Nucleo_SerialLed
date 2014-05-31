99a8f771df1496c1377c0dc4d4c8d9074b1a752c
carcon999 <carcon999@gmail.com>
2014-06-01 00:01:00 +0900

    ライセンスの記述修正

--- Lib/SerialLedController.h
+++ Lib/SerialLedController.h
@@ -7,19 +7,6 @@
   originally distributed at carcon999's Homepage. 
   <http://blogs.yahoo.co.jp/carcon999/folder/1250867.html>
 
-  SerialLEDController is free software: you can redistribute it and/or modify
-  it under the terms of the GNU Lesser General Public License as published by
-  the Free Software Foundation, either version 2.1 of the License, or
-  (at your option) any later version.
-
-  SerialLEDController is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public License
-  along with SerialLEDController.  If not, see <http://www.gnu.org/licenses/>.
-
 */
 
 #ifndef SERIALLEDCONTROLLER_H
